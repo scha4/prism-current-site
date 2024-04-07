42ecb1415a90600672214d45068164553b64dabe
hs95blue <hs95blue@gmail.com>
2024-04-07 18:31:18 +0900

    vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const defaultAuth = 'jwt';
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-      'REACT_APP_DEFAULTAUTH': JSON.stringify(defaultAuth),
-  }
+ 
 })
